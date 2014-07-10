cc9573b7f754703cb9dd9d3292743936902a0247
Steve Elliott <steve@tegud.net>
2014-07-10 22:32:28 +0100

    Correcting git attributes

--- test/index.js
+++ test/index.js
@@ -14,6 +14,11 @@ describe('SENTINEL.Composer', function() {
 
 		beforeEach(function(done) {
 			server = new Server({
+				memoryStore: {
+					maxInactivityUnits: 'ms',
+					maxInactivity: 20
+				},
+				refreshRate: 10,
 				listenOnPort: 1234,
 				emitOnPort: 1235
 			});
@@ -63,7 +68,7 @@ describe('SENTINEL.Composer', function() {
 			it('with specified sessionId', function(done) {
 				var testData = loadTestData('one.json');
 
-				sendTest(cloneData(testData), 100);
+				sendTest(cloneData(testData), 5);
 
 				udpClient.on("message", function messageReceived(msg) {
 					var data = msg.toString('utf-8');
@@ -77,7 +82,7 @@ describe('SENTINEL.Composer', function() {
 			it('sets number of errors encountered when session contains errors', function(done) {
 				var testData = loadTestData('errors.json');
 
-				sendTest(testData, 100);
+				sendTest(testData, 5);
 
 				udpClient.on("message", function messageReceived(msg) {
 					var data = msg.toString('utf-8');
@@ -91,7 +96,7 @@ describe('SENTINEL.Composer', function() {
 			it('sets booked to false if session does not contain conversion event', function(done) {
 				var testData = loadTestData('one.json');
 
-				sendTest(testData, 100);
+				sendTest(testData, 5);
 
 				udpClient.on("message", function messageReceived(msg) {
 					var data = msg.toString('utf-8');
@@ -105,7 +110,7 @@ describe('SENTINEL.Composer', function() {
 			it('sets booked to true if session contains conversion event', function(done) {
 				var testData = loadTestData('booking.json');
 
-				sendTest(testData, 100);
+				sendTest(testData, 5);
 
 				udpClient.on("message", function messageReceived(msg) {
 					var data = msg.toString('utf-8');
@@ -119,7 +124,7 @@ describe('SENTINEL.Composer', function() {
 			it('sets bookingDetails if session contains conversion event', function(done) {
 				var testData = loadTestData('booking.json');
 
-				sendTest(testData, 100);
+				sendTest(testData, 5);
 
 				udpClient.on("message", function messageReceived(msg) {
 					var data = msg.toString('utf-8');
@@ -151,73 +156,75 @@ describe('SENTINEL.Composer', function() {
 				});
 			});
 
-			it('sets user userAgent to value of last request', function(done) {
-				var testData = loadTestData('one.json');
-
-				sendTest(testData, 100);
-
-				udpClient.on("message", function messageReceived(msg) {
-					var data = msg.toString('utf-8');
-					var parsedData = JSON.parse(data);
-
-					expect(parsedData.user.userAgent).to.eql({
-						full: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-						name: "Googlebot",
-						os: "Other",
-						osName: "Other",
-						device: "Spider",
-						major: "2",
-						minor: "1"
-					});
-					done();
-				});
-			});
-
-			describe('sets user type', function() {
-				it('to GoodBot when last request was identified as a bot from UserAgent', function(done) {
+			describe('sets user', function() {
+				it('userAgent to value of last request', function(done) {
 					var testData = loadTestData('one.json');
 
-					sendTest(testData, 100);
+					sendTest(testData, 5);
 
 					udpClient.on("message", function messageReceived(msg) {
 						var data = msg.toString('utf-8');
 						var parsedData = JSON.parse(data);
 
-						expect(parsedData.user.type).to.be('GoodBot');
+						expect(parsedData.user.userAgent).to.eql({
+							full: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+							name: "Googlebot",
+							os: "Other",
+							osName: "Other",
+							device: "Spider",
+							major: "2",
+							minor: "1"
+						});
 						done();
 					});
 				});
 
-				it('to BadBot when last request was not identified as a bot from UserAgent and BotBuster score over 0', function(done) {
-					var testData = loadTestData('one.json');
+				describe('type', function() {
+					it('to GoodBot when last request was identified as a bot from UserAgent', function(done) {
+						var testData = loadTestData('one.json');
 
-					testData[0]['UA_is_bot'] = false;
-					testData[0]['botBuster_score'] = "1000";
+						sendTest(testData, 5);
 
-					sendTest(testData, 100);
+						udpClient.on("message", function messageReceived(msg) {
+							var data = msg.toString('utf-8');
+							var parsedData = JSON.parse(data);
 
-					udpClient.on("message", function messageReceived(msg) {
-						var data = msg.toString('utf-8');
-						var parsedData = JSON.parse(data);
-
-						expect(parsedData.user.type).to.be('BadBot');
-						done();
+							expect(parsedData.user.type).to.be('GoodBot');
+							done();
+						});
 					});
-				});
 
-				it('to Human when last request was not identified as a bot from UserAgent and BotBuster score of 0', function(done) {
-					var testData = loadTestData('one.json');
+					it('to BadBot when last request was not identified as a bot from UserAgent and BotBuster score over 0', function(done) {
+						var testData = loadTestData('one.json');
 
-					testData[0]['UA_is_bot'] = false;
+						testData[0]['UA_is_bot'] = false;
+						testData[0]['botBuster_score'] = "1000";
 
-					sendTest(testData, 100);
+						sendTest(testData, 5);
 
-					udpClient.on("message", function messageReceived(msg) {
-						var data = msg.toString('utf-8');
-						var parsedData = JSON.parse(data);
+						udpClient.on("message", function messageReceived(msg) {
+							var data = msg.toString('utf-8');
+							var parsedData = JSON.parse(data);
 
-						expect(parsedData.user.type).to.be('Human');
-						done();
+							expect(parsedData.user.type).to.be('BadBot');
+							done();
+						});
+					});
+
+					it('to Human when last request was not identified as a bot from UserAgent and BotBuster score of 0', function(done) {
+						var testData = loadTestData('one.json');
+
+						testData[0]['UA_is_bot'] = false;
+
+						sendTest(testData, 5);
+
+						udpClient.on("message", function messageReceived(msg) {
+							var data = msg.toString('utf-8');
+							var parsedData = JSON.parse(data);
+
+							expect(parsedData.user.type).to.be('Human');
+							done();
+						});
 					});
 				});
 			});
