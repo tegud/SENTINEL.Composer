d6aff2986e77e44493d539abe6fb7dd2cac0048f
Steve Elliott <steve@tegud.net>
2014-07-10 14:39:20 +0100

    Will record number of errors encountered

--- test/index.js
+++ test/index.js
@@ -32,22 +32,86 @@ describe('SENTINEL.Composer', function() {
 			eventEmitter.removeAllListeners();
 
 			server.stop().then(done);
+
+			server = null;
 		});
+
+		function sendTest(testData, gapBetween) {
+			var currentTestItem = JSON.stringify(testData.shift());
+			var message = new Buffer(currentTestItem);
+
+			udpClient.send(message, 0, message.length, port, "localhost", function() {
+				if(testData.length) {
+					setTimeout(function() {
+						sendTest(testData, gapBetween);
+					}, gapBetween);
+				}
+			});
+		}
+
+		function loadTestData(fileName) {
+			var testData = fs.readFileSync(__dirname + '/data/' + fileName, 'utf-8');
+
+			return JSON.parse(testData);
+		}
+
+		function cloneData(data) {
+			return JSON.parse(JSON.stringify(data));
+		}
 
 		describe('creates new session object', function() {
 			it('with specified sessionId', function(done) {
-				var testData = fs.readFileSync(__dirname + '/data/one.json', 'utf-8');
-				var parsedTestData = JSON.parse(testData);
+				var testData = loadTestData('one.json');
 
-				var message = new Buffer(JSON.stringify(parsedTestData[0]));
-
-				udpClient.send(message, 0, message.length, port, "localhost", function() {});
+				sendTest(cloneData(testData), 100);
 
 				udpClient.on("message", function messageReceived(msg, rinfo) {
 					var data = msg.toString('utf-8');
 					var parsedData = JSON.parse(data);
 
-					expect(parsedData.sessionId).to.eql(parsedTestData[0].sessionId);
+					expect(parsedData.sessionId).to.be(testData[0].sessionId);
+					done();
+				});
+			});
+
+			it('sets number of errors encountered when session contains errors', function(done) {
+				var testData = loadTestData('errors.json');
+
+				sendTest(testData, 100);
+
+				udpClient.on("message", function messageReceived(msg, rinfo) {
+					var data = msg.toString('utf-8');
+					var parsedData = JSON.parse(data);
+
+					expect(parsedData.errors).to.be(2);
+					done();
+				});
+			});
+
+			it('sets booked to false if session does not contain conversion event', function(done) {
+				var testData = loadTestData('one.json');
+
+				sendTest(testData, 100);
+
+				udpClient.on("message", function messageReceived(msg, rinfo) {
+					var data = msg.toString('utf-8');
+					var parsedData = JSON.parse(data);
+
+					expect(parsedData.booked).to.be(false);
+					done();
+				});
+			});
+
+			it('sets booked to true if session contains conversion event', function(done) {
+				var testData = loadTestData('booking.json');
+
+				sendTest(testData, 100);
+
+				udpClient.on("message", function messageReceived(msg, rinfo) {
+					var data = msg.toString('utf-8');
+					var parsedData = JSON.parse(data);
+
+					expect(parsedData.booked).to.be(false);
 					done();
 				});
 			});
