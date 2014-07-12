dc9c23e1ca6a4222faa80f9996e52440ee090686
Steve Elliott <steve@tegud.net>
2014-07-12 13:30:03 +0100

    No funnel requests no longer causes error

--- test/ComposedObjectFactories/requests.js
+++ test/ComposedObjectFactories/requests.js
@@ -3,7 +3,16 @@ var buildRequests = require('../../lib/ComposedObjectFactories/requests');
 
 describe('buildRequests', function() {
 	it('does not fail when there are no requests', function() {
-		expect(buildRequests([])).to.be(undefined);
+		expect(buildRequests({
+			events: []
+		})).to.be(undefined);
 	});
 
+	it('when there are no funnel requests, it sets the exitedFunnelAt to none', function() {
+		expect(buildRequests({ 
+			events: [
+				{ type: 'lr_varnish_request', url_page_type: 'anotherpage.mvc' }
+			]
+		}).funnelExitedAt).to.be('unknown');
+	});
 });
