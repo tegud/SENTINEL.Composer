2ae814765ca3250351b58493d2adfb74954c8703
Steve Elliott <steve@tegud.net>
2014-07-12 14:04:59 +0100

    Booking-confirmation and booking tracked
    Tracked via booking and booking errors for now

--- test/ComposedObjectFactories/requests.js
+++ test/ComposedObjectFactories/requests.js
@@ -15,4 +15,47 @@ describe('buildRequests', function() {
 			]
 		}).funnelExitedAt).to.be('unknown');
 	});
+
+	it('when last request is home, it sets the exitedFunnelAt to home', function() {
+		expect(buildRequests({ 
+			events: [
+				{ type: 'lr_varnish_request', url_page_type: 'home' }
+			]
+		}).funnelExitedAt).to.be('home');
+	});
+
+	it('when last request is hotel-details, it sets the exitedFunnelAt to hotel-details', function() {
+		expect(buildRequests({ 
+			events: [
+				{ type: 'lr_varnish_request', url_page_type: 'hotel-details' }
+			]
+		}).funnelExitedAt).to.be('hotel-details');
+	});
+
+	it('when last request is search, it sets the exitedFunnelAt to search', function() {
+		expect(buildRequests({ 
+			events: [
+				{ type: 'lr_varnish_request', url_page_type: 'search' }
+			]
+		}).funnelExitedAt).to.be('search');
+	});
+
+	it('when booking error has occurred, it sets the exitedFunelAt to booking', function() {
+		expect(buildRequests({ 
+			events: [
+				{ type: 'lr_varnish_request', url_page_type: 'search' },
+				{ type: 'lr_errors', url_page_type: 'booking' }
+			]
+		}).funnelExitedAt).to.be('booking');
+	});
+
+	it('when booking has completed error has occurred, it sets the exitedFunelAt to booking', function() {
+		expect(buildRequests({ 
+			events: [
+				{ type: 'lr_varnish_request', url_page_type: 'search' },
+				{ type: 'lr_errors', url_page_type: 'booking' },
+				{ type: 'domain_events', domainEventType: 'booking made' }
+			]
+		}).funnelExitedAt).to.be('booking-confirmation');
+	});
 });
