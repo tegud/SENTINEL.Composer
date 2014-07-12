0d566fe7eccc024435d8d409e71b06d4fd9a3db1
Steve Elliott <steve@tegud.net>
2014-07-12 22:18:34 +0100

    session started + length

--- test/ComposedObjectFactories/session.js
+++ test/ComposedObjectFactories/session.js
@@ -10,12 +10,31 @@ describe('buildSession', function() {
 		}).type).to.be('session');
 	});
 
+	it('sets started to timestamp of first event', function() {
+		expect(buildRequests({
+			events: [
+				{ '@timestamp': "2014-06-18T20:30:12.928Z" },
+				{ '@timestamp': "2014-06-18T20:36:12.928Z" }
+			]
+		})['started']).to.be("2014-06-18T20:30:12.928Z");		
+	});
+
 	it('sets @timestamp to last event', function() {
 		expect(buildRequests({
 			events: [
+				{ '@timestamp': "2014-06-18T20:30:12.928Z" },
 				{ '@timestamp': "2014-06-18T20:36:12.928Z" }
 			]
 		})['@timestamp']).to.be("2014-06-18T20:36:12.928Z");
+	});
+
+	it('sets length to time in milliseconds between first and last event', function() {
+		expect(buildRequests({
+			events: [
+				{ '@timestamp': "2014-06-18T20:36:12.928Z" },
+				{ '@timestamp': "2014-06-18T20:47:14.111Z" }
+			]
+		}).length).to.be(661183);
 	});
 
 	it('sets sessionId', function() {
