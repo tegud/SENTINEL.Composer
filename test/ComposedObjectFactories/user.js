78da2cd416bb00b23ecee898a2f0d068616253da
Steve Elliott <steve@tegud.net>
2014-08-14 13:17:46 +0100

    Correcting type setting for users
    Checks all requests, not just the last

--- test/ComposedObjectFactories/user.js
+++ test/ComposedObjectFactories/user.js
@@ -1,5 +1,33 @@
 var expect = require('expect.js');
-var buildRequests = require('../../lib/ComposedObjectFactories/session');
+var buildUser = require('../../lib/ComposedObjectFactories/user');
 
 describe('buildUser', function() {
+	describe('sets user.type', function() {
+		it('to GoodBot if any request User Agent identifies it as a bot', function() {
+			expect(buildUser({ 
+				events: [
+					{ type: 'lr_varnish_request', req_headers: {}, UA_is_bot: "true" },
+					{ type: 'lr_varnish_request', req_headers: {} }
+				]
+			}).type).to.be('GoodBot');
+		});
+
+		it('to BadBot if any request Bot Buster score identifies it as a bot', function() {
+			expect(buildUser({ 
+				events: [
+					{ type: 'lr_varnish_request', req_headers: {}, botBuster_score: "150" },
+					{ type: 'lr_varnish_request', req_headers: {} }
+				]
+			}).type).to.be('BadBot');
+		});
+
+		it('to Human if no request User Agent or Bot Buster Score identifies it as a bot', function() {
+			expect(buildUser({ 
+				events: [
+					{ type: 'lr_varnish_request', req_headers: {} },
+					{ type: 'lr_varnish_request', req_headers: {} }
+				]
+			}).type).to.be('Human');
+		});
+	});
 });
