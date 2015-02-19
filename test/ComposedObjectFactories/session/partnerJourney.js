456b8ddcda41ee9f16cacd95692009caa9de0657
Danny Smith <danny.smith@laterooms.com>
2015-02-19 15:18:56 +0000

    Log if the partner code has just changed

--- test/ComposedObjectFactories/session/partnerJourney.js
+++ test/ComposedObjectFactories/session/partnerJourney.js
@@ -8,73 +8,113 @@ describe('buildPartnerJourney', function () {
         })).to.eql({});
     });
 
-    it('logs unique partner codes in the requests', function () {
-        var request = buildPartnerJourney({
-            events: [{
-                "type": "lr_varnish_request"
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=1301"
-                }
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=1301"
-                }
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=2398"
-                }
-            }]
+    describe('logging partner codes', function () {
+        it('logs unique partner codes in the requests', function () {
+            var partnerJourney = buildPartnerJourney({
+                events: [{
+                    "type": "lr_varnish_request"
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=2398"
+                    }
+                }]
+            });
+            expect(partnerJourney.partnerCodeOrder).to.eql('NOPARTNERCODE,partner=1301,partner=2398');
+            expect(partnerJourney.partnerCodeNumber).to.eql(3);
         });
-        expect(request.partnerCodeOrder).to.eql('NOPARTNERCODE, partner=1301, partner=2398');
-        expect(request.partnerCodeNumber).to.eql(3);
+
+        it('log partner codes in the order they were set', function () {
+            var partnerJourney = buildPartnerJourney({
+                events: [{
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=2398"
+                    }
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }]
+            });
+            expect(partnerJourney.partnerCodeOrder).to.eql('partner=1301,partner=2398,partner=1301');
+            expect(partnerJourney.partnerCodeNumber).to.eql(3);
+        });
+
+        it('logs "NOPARTNERCODE" as a partner code if the partner code header disappears', function () {
+            var partnerJourney = buildPartnerJourney({
+                events: [{
+                    "type": "lr_varnish_request"
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }, {
+                    "type": "lr_varnish_request"
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=2398"
+                    }
+                }]
+            });
+            expect(partnerJourney.partnerCodeOrder).to.eql('NOPARTNERCODE,partner=1301,NOPARTNERCODE,partner=2398');
+            expect(partnerJourney.partnerCodeNumber).to.eql(4);
+        });
     });
 
-    it('log partner codes in the order they were set', function () {
-        var request = buildPartnerJourney({
-            events: [{
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=1301"
-                }
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=2398"
-                }
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=1301"
-                }
-            }]
+    describe('partnerCodeChanged', function () {
+        it('logs partnerCodeChanged as true if the partner code has changed from the previous request', function () {
+            expect(buildPartnerJourney({
+                events: [{
+                    "type": "lr_varnish_request"
+                }, {
+                    "type": "lr_varnish_request"
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }]
+            }).partnerCodeChanged).to.eql(true);
         });
-        expect(request.partnerCodeOrder).to.eql('partner=1301, partner=2398, partner=1301');
-        expect(request.partnerCodeNumber).to.eql(3);
-    });
 
-    it('logs "NOPARTNERCODE" as a partner code if the partner code header disappears', function () {
-        var request = buildPartnerJourney({
-            events: [{
-                "type": "lr_varnish_request"
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=1301"
-                }
-            }, {
-                "type": "lr_varnish_request"
-            }, {
-                "type": "lr_varnish_request",
-                "resp_headers": {
-                    "x_log_partner": "partner=2398"
-                }
-            }]
+        it('logs partnerCodeChanged as false if the partner code has not changed from the previous request', function () {
+            expect(buildPartnerJourney({
+                events: [{
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }, {
+                    "type": "lr_varnish_request",
+                    "resp_headers": {
+                        "x_log_partner": "partner=1301"
+                    }
+                }]
+            }).partnerCodeChanged).to.eql(false);
         });
-        expect(request.partnerCodeOrder).to.eql('NOPARTNERCODE, partner=1301, NOPARTNERCODE, partner=2398');
-        expect(request.partnerCodeNumber).to.eql(4);
     });
 });
